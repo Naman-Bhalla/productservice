0b1e0b0f0fc57fecef586f0a9ae9b8bdaaa58b18
Vishal Nawale <vnawale@reliaquest.com>
2023-10-02 19:02:25 +0530

    7 Api implementation

--- src/main/java/dev/naman/productservice/repositories/CategoryRepository.java
+++ src/main/java/dev/naman/productservice/repositories/CategoryRepository.java
@@ -1,19 +1,15 @@
 package dev.naman.productservice.repositories;
 
 import dev.naman.productservice.models.Category;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Lazy
-public interface CategoryRepository
-extends JpaRepository<Category, UUID> {
-
-    Optional<Category> findById(UUID uuid);
-
-    @Override
-    List<Category> findAllById(Iterable<UUID> uuids);
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    public <S extends Category> S save(S entity);
+    public Optional<Category> findById(UUID uuid);
+    public Optional<Category> findByName(String name);
 }

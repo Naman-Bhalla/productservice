b07c980ce1c467e082d72166c78fe0b7c7933607
deveshjeewani <deveshjeewani@gmail.com>
2023-10-02 21:03:41 +0530

    commit and push for Naman end Unit test-2

--- src/main/java/dev/naman/productservice/repositories/PriceRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.naman.productservice.repositories;
-
-import dev.naman.productservice.models.Price;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PriceRepository
-extends JpaRepository<Price, Long> {
-}
